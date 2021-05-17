5a5d5d14878e0fc8822d5ef8285ef890881fecf7
sina hassani <mohammadsinahassani@gmail.com>
2021-05-17 10:29:17 -0400

    first.py

--- first.py
+++ first.py
@@ -1,1 +1,26 @@
+import datetime
+import pickle
 
+
+class Book:
+    ISBN: int
+    name: str
+    author: str
+    publisher: str
+    publish_date: datetime.date
+
+    def __init__(self, ISBN, name, author, publisher, publish_date):
+        self.ISBN = ISBN
+        self.name = name
+        self.author = author
+        self.publisher = publisher
+        self.publish_date = publish_date
+
+    @classmethod
+    def unpickle_file(cls):
+        cls.book_list = pickle.load(open("books.pkl"))
+        return cls.book_list
+
+
+books = Book.unpickle_file()
+print(books)
