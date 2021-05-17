823751eed66c0609974d75355bb8b2ae6734679b
sina hassani <mohammadsinahassani@gmail.com>
2021-05-17 11:33:35 -0400

    hello

--- third.py
+++ third.py
@@ -1,1 +1,37 @@
 
+def swapcase_decorator(gen):
+    def inner_gen(*args, **kwargs):
+        value = gen(*args)
+        value = str(value)
+        value = value.swapcase()
+        yield value
+
+
+    return inner_gen
+
+
+def check_duplicate(word):
+    digit_list = []
+    word = str(word)
+    for i in word:
+        if i not in digit_list:
+            digit_list.append(i)
+        else:
+            return False
+    return True
+
+
+@swapcase_decorator
+def duplicate_words_gen(file_path):
+    file = open(file_path)
+    content = file.read()
+    words_list = content.split()
+    for word in words_list:
+        if check_duplicate(word):
+            yield word
+
+
+if __name__ == '__main__':
+    for i in duplicate_words_gen("hello.txt"):
+        print(i)
+
